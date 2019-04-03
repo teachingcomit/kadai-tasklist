471420b8ae92eb7f4795fcb8af3add58138be927
tesutesu <testerman178@gmail.com>
2019-04-03 20:39:10 +0900

    Add NewServlet.java

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,8 +1,7 @@
-
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
+import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -15,16 +14,16 @@ import models.Task;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,14 +33,23 @@ public class IndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		EntityManager em = DBUtil.createEntityManager();
+		em.getTransaction().begin();
 
-	List<Task> tasks = em.createNamedQuery("getAllTasks",Task.class) .getResultList();
+		Task t = new Task();
 
+		String content = "hello";
+		t.setContent(content);
 
-		response.getWriter().append("Served at: ").append(Integer.valueOf(tasks.size()).toString());
+		Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+		t.setCreated_at(currentTime);
+		t.setUpdated_at(currentTime);
 
-		em.close();
+		em.persist(t);
+		em.getTransaction().commit();
 
+		response.getWriter().append(Integer.valueOf(t.getId()).toString());
+
+	em.close();
 	}
 
 }
